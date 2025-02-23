280cd30dff2dd1b98ddb05ff097883b1f4a481e7
Virat Srivastava <vsrivastava.dev@gmail.com>
2025-02-23 15:03:02 +0530

    project : bulb onOFF

--- Projects in js/Challenge 1 to 5/challenge-1/app.js
+++ Projects in js/Challenge 1 to 5/challenge-1/app.js
@@ -1,3 +1,35 @@
 /**
  * Write your challenge solution here
  */
+const buttonElement = ç("#toggleButton");
+const imgElement = document.querySelector("#bulb");
+const statusElement = document.querySelector('#status')
+
+
+
+function turnOnOff() {
+    
+  if (imgElement.className === "bulb on") {
+
+    imgElement.className = "bulb off";
+    document.body.classList.remove("dark-mode");
+    buttonElement.innerHTML = "Turn On";
+    statusElement.innerHTML = "Status: Off"
+
+  } else {
+
+    imgElement.className = "bulb on";
+    document.body.classList.add("dark-mode");
+    buttonElement.innerHTML = "Turn Off";
+    statusElement.innerHTML = "Status: On"
+    
+
+  }
+}
+
+buttonElement.addEventListener('click', turnOnOff);
+/**
+ * Learnings - 
+ * .classlist returns css classnames of the element that we can manipulate
+ * document.querySelector returns the whole HTML element
+ */
