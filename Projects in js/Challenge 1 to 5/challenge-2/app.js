62e736977acd9c6f6d649944ae0ae2c8c27778a3
Virat Srivastava <vsrivastava.dev@gmail.com>
2025-02-25 07:59:22 +0530

    challenge 2 : completed

--- Projects in js/Challenge 1 to 5/challenge-2/app.js
+++ Projects in js/Challenge 1 to 5/challenge-2/app.js
@@ -1,3 +1,39 @@
 /**
  * Write your challenge solution here
  */
+// one way is to directly get all elements by id and then individual pe onClick.
+
+const colorButtonsContainer = document.querySelector('.color-buttons')
+const mainHeading = document.getElementById('mainHeading');
+function changeColor(color) {
+    
+    const mainHeading2 = document.querySelector('#mainHeading');
+    // mainHeading2.classList.remove('red', 'green', 'blue');
+    // mainHeading2.classList.add(`color-${color}`)
+    //whats the difference between style.color and classlist ?
+    mainHeading.style.color = color;
+}
+
+colorButtonsContainer.addEventListener('click',function(event){
+    if(event.target.tagName.toLowerCase() === 'button') {
+        const buttonId = event.target.id
+
+        switch(buttonId) {
+            case 'redButton':
+                changeColor('red')
+                break
+            case 'greenButton':
+                changeColor('green')
+                break
+            case 'blueButton':
+                changeColor('blue')
+                break
+            case 'purpleButton':
+                changeColor('purple')
+                break
+            case 'resetButton':
+                changeColor('')
+                break
+        }
+    }
+}) 
