f8d8e85edb18dd61374fa8956ee2eb3af6fb83c8
Virat Srivastava <vsrivastava.dev@gmail.com>
2025-02-27 17:22:05 +0530

    feature : challenge 3 done

--- Projects in js/Challenge 1 to 5/challenge-3/app.js
+++ Projects in js/Challenge 1 to 5/challenge-3/app.js
@@ -1,3 +1,26 @@
 /**
  * Write your challenge solution here
  */
+/**
+ * Approach 1: get the id of all the tags
+ * Write onChange for all the ids seperatly.
+ * Other way is to dynamically do it.
+ */
+
+const inputs= document.querySelectorAll('input,textArea')
+
+function updatePreview(element) {
+    const label = element.previousElementSibling.textContent.trim().slice(0,-1).toLowerCase()
+    
+    const value = element.value
+
+    const span = document.getElementById(`${label}Display`)
+    if(span) {
+        span.textContent = value ? value : 'Not Provided'
+    }
+
+}
+
+inputs.forEach(input => {
+    input.addEventListener('input',() => {updatePreview(input)})
+})
